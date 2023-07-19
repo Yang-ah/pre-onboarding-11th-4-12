0804e1d703b73f3bc6e1ecfd151ac54700f29efc
Yang-ah <yangah0409@gmail.com>
2023-07-19 11:25:23 +0900

    Design: SearchBar Main, public title&favicon, theme

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useGetKeywords, useHandleKeydown } from '../../hooks';
+import { IconSearch } from '../assets';
 
 const SearchBar = () => {
   const [label, setLabel] = useState({ input: '', keyword: '' });
@@ -33,18 +34,22 @@ const SearchBar = () => {
   }, [keywords, index]);
 
   return (
-    <Main>
-      <div>
-        <label>
+    <Wrap>
+      <Main>
+        <Label>
+          <IconSearch />
           <MainInput
             name="keywordInput"
             onChange={changeKeyword}
             onKeyDown={onKeyDown}
+            placeholder="질환명을 입력해주세요."
           />
           <p>{label.keyword}</p>
-        </label>
-        <button>검색</button>
-      </div>
+        </Label>
+        <Button>
+          <IconSearch />
+        </Button>
+      </Main>
       <h3>추천검색어</h3>
       {/**TODO: Remove inline style */}
       <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -59,7 +64,7 @@ const SearchBar = () => {
           ))}
         {!arrayLength && <div>추천 검색어가 없습니다</div>}
       </div>
-    </Main>
+    </Wrap>
   );
 };
 
@@ -73,9 +78,61 @@ const Input = styled.input`
   }
 `;
 
-const Main = styled.main`
+const Wrap = styled.div`
   width: 490px;
 `;
 
-const MainInput = styled.input``;
+const Main = styled.main`
+  position: relative;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  right: 8px;
+  top: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  background-color: ${props => props.theme.BLUE_30};
+  transform: translateY(-50%);
+
+  > svg {
+    width: 21px;
+    height: 21px;
+    fill: white;
+  }
+`;
+
+const MainInput = styled.input`
+  background-color: transparent;
+  width: 100%;
+  font-size: 18px;
+  //  user-select: none;
+`;
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  width: 490px;
+  height: 70px;
+  border: 2px solid white;
+  border-radius: 42px;
+  background-color: white;
+  padding: 20px 10px 20px 24px;
+
+  > svg {
+    width: 16px;
+    height: 16px;
+    margin-right: 12px;
+    fill: ${props => props.theme.GREY_20};
+  }
+
+  &:focus-within {
+    border-color: ${props => props.theme.BLUE_30};
+  }
+`;
+
 export default SearchBar;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,6 @@
 export const theme = {
-  GREY: '#D9D9D9',
-
+  GREY_10: '#D9D9D9',
+  GREY_20: '#A7AFB7',
   BLUE_10: '#EFF8FF',
   BLUE_20: '#CBE9FE',
   BLUE_30: '#147EE6',
